1e9076e7636c8977fcfcd4ad20a4ffa8a2919d54
lukekolar <60484336+lukekolar@users.noreply.github.com>
2020-09-16 14:23:19 -0500

    moved from corrupt directory to new folder

---
+++ discovering_discovery_data/data_scraping.R
@@ -0,0 +1,13 @@
+
+
+library(tidyverse)
+library(janitor)
+
+
+install.packages("geniusr")
+library(geniusr)
+genius_token("A629GZJinyQQJN11vDjRf_3tjx6Dsw5fpH1hdiLZwhpgABHw4yyTpm6nFQj592p8")
+
+
+
+
